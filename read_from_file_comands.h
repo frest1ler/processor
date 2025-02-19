bbc99ec7e0956a14e66a7388f432ee9deb913878
frest1ler <143139569+frest1ler@users.noreply.github.com>
2025-02-19 16:22:58 +0300

    Delete read_from_file_comands.h

--- read_from_file_comands.h
+++
@@ -1,9 +0,0 @@
-#ifndef READ_FROM_FILE_COMANDS_H
-#define READ_FROM_FILE_COMANDS_H
-
-#include "run.h"
-
-void read_from_file_comands(Info_about_text* info);
-void initialize_pointer_array(Info_about_text* info);
-
-#endif /*READ_FROM_FILE_COMANDS_H*/
